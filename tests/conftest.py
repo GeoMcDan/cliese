0c88c0151f98d64090e56bff652ae147df21207f
George <george.mcdaniel@gmail.com>
2025-10-21 20:18:39 -0400

    fix typer output colors

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,9 @@ import os
 
 os.environ["NO_COLOR"] = "1"
 os.environ["PY_COLORS"] = "0"
+os.environ["NO_COLOR"] = "1"
+os.environ["PY_COLORS"] = "0"
+os.environ["CLICOLOR"] = "0"
+os.environ["CLICOLOR_FORCE"] = "0"
+os.environ["RICH_NO_COLOR"] = "1"
+os.environ["RICH_FORCE_COLOR"] = "0"
