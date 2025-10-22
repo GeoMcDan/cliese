b25f5637f2ab5b3503ed579bc212ddb6561123aa
George <george.mcdaniel@gmail.com>
2025-10-21 20:10:52 -0400

    Trying to disable colors for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,5 @@
-import os
+def pytest_load_initial_conftests(args, early_config, parser):
+    import os
 
-import pytest
-
-
-@pytest.fixture(autouse=True, scope="session")
-def disable_color_output():
-    """
-    Disable ANSI color codes globally for Typer/Click during tests.
-    """
-    # Click and Typer both honor NO_COLOR if set.
     os.environ["NO_COLOR"] = "1"
+    os.environ["PY_COLORS"] = "0"
